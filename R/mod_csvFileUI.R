9dc95198e37b2000c10e3a80d93f3950425714be
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-09-03 19:14:46 -0500

    Quité campos innecesarios.

--- R/mod_csvFileUI.R
+++ R/mod_csvFileUI.R
@@ -12,19 +12,6 @@ mod_csvFileUI_ui <- function(id, label = "Selecciona el archivo"){
   
   tagList(
     fileInput(ns("file"), label),
-    checkboxInput(ns("heading"), "¿Tiene encabezado?", value=TRUE),
-    selectInput(ns("quote"), "Quote", c(
-      "Double quote" = "\"",
-      "Single quote" = "'",
-      "None" = ""
-      
-    )
-    ),
-    selectInput(ns("separator"), "Separador", c(
-      "Comma" = ",",
-      "Semmicolon" = ";"
-    )
-    ),
     selectInput(ns("encoding"), "Codificación", c(
       "UTF-8" = "UTF-8",
       "Latin-1" = "latin1"
@@ -56,23 +43,28 @@ mod_csvFileUI_server <- function(input, output, session){
   dataframe <- reactive({
     if(input$database == "fgj"){
       df <- readr::read_delim(userFile()$datapath,
-                              col_names = input$heading,
-                              quote = input$quote,
-                              delim = input$separator
+                              col_names = TRUE,
+                              quote = "\"",
+                              delim = ","
       )
       validate(need(try(preprocesa_pgj(df)), "El archivo no es de la FGJ"))
       df <- preprocesa_pgj(df)  
     }else if (input$database == "ssc"){
       df <- readr::read_delim(userFile()$datapath, 
-                        col_names = input$heading,
-                        quote = input$quote,
-                        delim=input$separator,
+                        col_names = TRUE,
+                        quote = "\"",
+                        delim=",",
                         col_types=list( "No. FOLIO" = readr::col_character(),
                                         "HORA_EVENTO" = readr::col_character())
                         )
       validate(need(try(preprocesa_ssc(df)), "El archivo no es de la SSC"))
       df <- preprocesa_ssc(df)
     }else if (input$database == "axa"){
+      df <- readr::read_delim(userFile()$datapath,
+                              col_names = TRUE,
+                              quote = "\"",
+                              delim = ";"
+      )
       validate(need(try(preprocesa_axa(df)), "El archivo no es de AXA"))
       df <- preprocesa_axa(df)
     }
