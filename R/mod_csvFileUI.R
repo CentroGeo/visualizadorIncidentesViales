00d9447f1323daa9f640397bca4df07910be75b4
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-09-09 16:33:08 -0500

    Formato de código.

--- R/mod_csvFileUI.R
+++ R/mod_csvFileUI.R
@@ -4,34 +4,31 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
-mod_csvFileUI_ui <- function(id, label = "Selecciona el archivo"){
+#' @importFrom shiny NS tagList
+mod_csvFileUI_ui <- function(id, label = "Selecciona el archivo") {
   ns <- NS(id)
-  
+
   tagList(
     fileInput(ns("file"), label),
     selectInput(ns("encoding"), "Codificación", c(
       "UTF-8" = "UTF-8",
       "Latin-1" = "latin1"
-    )
-    ),
+    )),
     selectInput(ns("database"), "Fuente de Datos", c(
       "Fiscalía" = "fgj",
       "Seguridad Ciudadana" = "ssc",
       "AXA" = "axa"
-    )
-    ),
+    )),
     actionButton(ns("save"), "Guardar")
   )
-  
 }
-    
+
 #' csvFileUI Server Function
 #'
-#' @noRd 
-mod_csvFileUI_server <- function(input, output, session){
+#' @noRd
+mod_csvFileUI_server <- function(input, output, session) {
   ns <- session$ns
   shinyjs::toggleState("save")
   userFile <- reactive({
@@ -39,61 +36,61 @@ mod_csvFileUI_server <- function(input, output, session){
     validate(need(input$file, message = FALSE))
     input$file
   })
-  
+
   # The user's data, parsed into a data frame
   dataframe <- reactive({
-    if(input$database == "fgj"){
+    if (input$database == "fgj") {
       df <- readr::read_delim(userFile()$datapath,
-                              col_names = TRUE,
-                              quote = "\"",
-                              delim = ","
+        col_names = TRUE,
+        quote = "\"",
+        delim = ","
       )
       validate(need(try(preprocesa_pgj(df)), "El archivo no es de la FGJ"))
       shinyjs::toggleState("save")
       df <- preprocesa_pgj(df)
-    }else if (input$database == "ssc"){
-      df <- readr::read_delim(userFile()$datapath, 
-                        col_names = TRUE,
-                        quote = "\"",
-                        delim=",",
-                        col_types=list( "No. FOLIO" = readr::col_character(),
-                                        "HORA_EVENTO" = readr::col_character())
-                        )
+    } else if (input$database == "ssc") {
+      df <- readr::read_delim(userFile()$datapath,
+        col_names = TRUE,
+        quote = "\"",
+        delim = ",",
+        col_types = list(
+          "No. FOLIO" = readr::col_character(),
+          "HORA_EVENTO" = readr::col_character()
+        )
+      )
       validate(need(try(preprocesa_ssc(df)), "El archivo no es de la SSC"))
       shinyjs::toggleState("save")
       df <- preprocesa_ssc(df)
-    }else if (input$database == "axa"){
+    } else if (input$database == "axa") {
       df <- readr::read_delim(userFile()$datapath,
-                              col_names = TRUE,
-                              quote = "\"",
-                              delim = ";"
+        col_names = TRUE,
+        quote = "\"",
+        delim = ";"
       )
       validate(need(try(preprocesa_axa(df)), "El archivo no es de AXA"))
       shinyjs::toggleState("save")
       df <- preprocesa_axa(df)
     }
   })
-  
+
   # We can run observers in here if we want to
   observe({
     msg <- sprintf("File %s was uploaded", userFile()$name)
     cat(msg, "\n")
   })
-  
+
   observeEvent(input$save, {
-    f_name <- paste0(input$database,".rds")
+    f_name <- paste0(input$database, ".rds")
     saveRDS(dataframe(), file = paste0("data-raw/", f_name))
     shinyjs::toggleState("save")
     showNotification("Datos guardados", type = "message")
   })
   # Return the reactive that yields the data frame
   return(dataframe)
- 
 }
-    
+
 ## To be copied in the UI
 # mod_csvFileUI_ui("csvFileUI_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_csvFileUI_server, "csvFileUI_ui_1")
- 
