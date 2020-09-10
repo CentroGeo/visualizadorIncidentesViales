722a5c26a0d2db4711a242fa788e9a2a177a3bf1
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-09-10 09:14:28 -0500

    Acción del botón de cambiar tab.

--- R/app_server.R
+++ R/app_server.R
@@ -4,9 +4,30 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
+app_server <- function(input, output, session) {
+  output$menu <- shinydashboard::renderMenu({
+    shinydashboard::sidebarMenu(id = "tabs",
+                shinydashboard::menuItem(
+                  "Introducción",
+                  icon = icon("door-open"),
+                  tabName = "intro"
+                ),
+                shinydashboard::menuItem(
+                  "Bases de Datos",
+                  icon = icon("layer-group"),
+                  tabName = "bd"
+                ),
+                shinydashboard::menuItem(
+                  "Visualizador",
+                  icon = icon("globe"),
+                  tabName = "visualizador"
+                )
+    )
+  })
+  callModule(mod_introPageUI_server, "introPageUI_ui_1", parent = session)
+
   # List the first level callModules here
-  callModule(mod_landingPageUI_server, "landing")
+  #callModule(mod_landingPageUI_server, "landing")
   # data <- callModule(mod_csvFileUI_server, "csvFileUI_ui")
   # output$tabla <- DT::renderDataTable({data()})
   # save <- callModule(mod_saveDataUI_server, "saveDataUI_ui", datos = data)
--- R/app_ui.R
+++ R/app_ui.R
@@ -7,12 +7,19 @@
 
 app_ui <- function(request) {
   shinydashboard::dashboardPage(
-    title = 'Visualizador de Datos de Incidentes Viales - SEMOVI',
-    skin = 'green',
-    header = shinydashboard::dashboardHeader(title = "Visualizador de Datos de Incidentes Viales - SEMOVI"),
-    sidebar = shinydashboard::dashboardSidebar(mod_landingPageSidebar_ui("landing")),
-    body = shinydashboard::dashboardBody(mod_landingPageBody_ui("landing"))
-    )
+    # title = 'Visualizador de Datos de Incidentes Viales - SEMOVI',
+    # skin = 'green',
+    shinydashboard::dashboardHeader(
+      title = "Visualizador de Datos de Incidentes Viales - SEMOVI"),
+    shinydashboard::dashboardSidebar(shinydashboard::sidebarMenuOutput("menu")),
+    shinydashboard::dashboardBody(shinydashboard::tabItems(
+      shinydashboard::tabItem(tabName = "intro",
+        mod_introPageUI_ui("introPageUI_ui_1")
+      ),
+      shinydashboard::tabItem(tabName = "bd", h1("Tab Two")),
+      shinydashboard::tabItem(tabName = "visualizador", h1("Tab Three"))
+    )),
+  )
 }
 
 #' Add external Resources to the Application
@@ -24,12 +31,14 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  add_resource_path('www', app_sys('app/www'))
-  
+  add_resource_path("www", app_sys("app/www"))
+
   tags$head(
     favicon(),
-    bundle_resources(path = app_sys('app/www'),
-                     app_title = 'package'),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "package"
+    ),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
     shinyjs::useShinyjs(debug = TRUE)
