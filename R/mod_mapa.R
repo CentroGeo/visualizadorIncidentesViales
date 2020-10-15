8423e35f84ee3e1f9c391e75852d9c097f257a31
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-10-15 12:42:02 -0500

    Documentación.

--- R/mod_mapa.R
+++ R/mod_mapa.R
@@ -7,7 +7,7 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-#' 
+#'
 #' @return a map
 mod_mapa_ui <- function(id) {
   ns <- NS(id)
@@ -19,20 +19,22 @@ mod_mapa_ui <- function(id) {
 
 #' mapa Server Function
 #'
-#' Map server gets the map from openstreetmaps an zoom it acordandly 
-#' 
+#' Despliega y actualiza el mapa de acuerdo a la selección del usuario.
+#'
 #' @param input shiny Parameter where the inputs from the UI are store
-#' 
-#' @param output shiny parameter where the id are reference to display in the UI  
-#' 
-#' @param session shiny parameter to store the session 
-#' 
-mod_mapa_server <- function( input, output, session, datos ){
+#'
+#' @param output shiny parameter where the id are
+#'               reference to display in the UI
+#'
+#' @param session shiny parameter to store the session
+#'
+#' @param datos reactive dataframe with data filterd by user parameters
+mod_mapa_server <- function(input, output, session, datos) {
   cdmx <- sf::read_sf(dsn = "./data/cdmx.shp", layer = "cdmx")
   centroides <- sf::st_transform(cdmx, 32614) %>%
     sf::st_centroid(.) %>%
     sf::st_transform(., "+proj=longlat +ellps=GRS80 +no_defs")
-  #######  To set map
+  # Fijar parámetros iniciales y desplegar el mapa base
   lon <- -99.152613
   lat <- 19.320497
   zoom <- 10
@@ -46,6 +48,7 @@ mod_mapa_server <- function( input, output, session, datos ){
               color = "#006738",
               opacity = 0.75)
   output$myMap <- leaflet::renderLeaflet({map})
+  # Íconos para los incidentes
   icons <- leaflet::iconList(
     DECESO_FGJ = leaflet::makeIcon("www/pgj_a.png"),
     LESIONADO_FGJ = leaflet::makeIcon("www/pgj_b.png"),
@@ -57,17 +60,23 @@ mod_mapa_server <- function( input, output, session, datos ){
     LESIONADO_AXA = leaflet::makeIcon("www/axa_b.png"),
     ACCIDENTE_AXA = leaflet::makeIcon("www/axa_c.png")
   )
+  # Observamos los cambios en input de usuario
   observeEvent(datos[[1]](), ignoreNULL = FALSE, {
+    # transformamos a coordenadas geográficas
     datos_4326 <- sf::st_transform(datos[[1]](), "+init=epsg:4326")
+    # columna para saber qué icono usar
     datos_4326 <- dplyr::mutate(datos_4326,
       icon_class = paste(tipo_incidente, fuente, sep = "_")
     )
+    # si tenemos alcaldía seleccionada, centramos el mapa y hacemos zoom
     if (datos[[2]]() != "Total Ciudad de México") {
        alcaldia <- dplyr::filter(centroides, nom_mun == datos[[2]]())
        lon <- sf::st_coordinates(alcaldia)[1]
        lat <- sf::st_coordinates(alcaldia)[2]
        zoom <- 12
     }
+    # Agragamos la capa de markers con los
+    # incidentes seleccionados por el usuario
     leaflet::leafletProxy("myMap") %>%
       leaflet::clearMarkers() %>%
       leaflet::clearMarkerClusters() %>%
