9a5572991c4f3c68beabcd131cba05a75b21eb44
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-10-15 12:31:28 -0500

    Formato de código.

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,28 +12,26 @@ app_ui <- function(request) {
     shinydashboard::dashboardHeader(
       title = "Visualizador de Datos de Incidentes Viales - SEMOVI"),
     shinydashboard::dashboardSidebar(shinydashboard::sidebarMenuOutput("menu")),
-    
     shinydashboard::dashboardBody(shinydashboard::tabItems(
-	##############Introduccion##########
+  ##############Introduccion##########
       shinydashboard::tabItem(tabName = "intro",
         mod_introPageUI_ui("introPageUI_ui_1")
       ),
       shinydashboard::tabItem(tabName = "bd", mod_infoBdUI_ui("infoBdUI_ui_1")),
       ########### Visualizador#################
       shinydashboard::tabItem(
-			                    tabName = "visualizador",
-      				            fluidPage(
-                				     column(6,
-                				       mod_mapa_ui("mapa_ui_1"),
-                				       mod_bar_ui("bar_ui_1")
-                				     ),
-                				     
-                				     column(6,
-				                        mod_DBSelector_ui("DBSelector_ui_1"),
-				                        mod_graficas_ui("graficas_ui_1")
-				                    )
-				                )
-		                ),
+                          tabName = "visualizador",
+                          fluidPage(
+                             column(6,
+                               mod_mapa_ui("mapa_ui_1"),
+                               mod_bar_ui("bar_ui_1")
+                             ),
+                             column(6,
+                                mod_DBSelector_ui("DBSelector_ui_1"),
+                                mod_graficas_ui("graficas_ui_1")
+                            )
+                        )
+                    ),
       ######## Actualizacion########
       shinydashboard::tabItem(tabName = "actualiza",
         mod_csvFileUI_ui("csvFileUI_ui", label = "Selecciona un CSV"),
