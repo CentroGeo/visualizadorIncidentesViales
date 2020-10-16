2d081ca831997b87f64eb97275a5b3a8fe64610a
plablo09 <pablo.lopez@centrogeo.edu.mx>
2020-10-16 10:14:54 -0500

    Formato de código.

--- R/mod_bar.R
+++ R/mod_bar.R
@@ -1,41 +1,42 @@
 #' bar UI Function
-#' 
-#' Generate a bar to get a time interval 
-#' 
+#'
+#' Generate a bar to get a time interval
+#'
 #'
 #' @param id Internal parameters for {shiny}.
 #'
 #'
-#' @importFrom shiny NS tagList 
-#' 
-mod_bar_ui <- function(id){
+#' @importFrom shiny NS tagList
+#'
+mod_bar_ui <- function(id) {
   ns <- NS(id)
   fluidRow(
-    sliderInput(inputId = ns('filtro_fecha') ,
-                label = 'Periodo de Tiempo' ,
-                width = '100%',
-                timeFormat = '%d/%m/%Y',
-                min = as.Date("2015-01-01","%Y-%m-%d"),
-                max = as.Date("2019-12-31","%Y-%m-%d"),
-                value = c(as.Date('2018-01-01',"%Y-%m-%d") , as.Date('2019-12-31',"%Y-%m-%d"))
+    sliderInput(inputId = ns("filtro_fecha"),
+                label = "Periodo de Tiempo",
+                width = "100%",
+                timeFormat = "%d/%m/%Y",
+                min = as.Date("2015-01-01", "%Y-%m-%d"),
+                max = as.Date("2020-09-09", "%Y-%m-%d"),
+                value = c(as.Date("2018-01-01", "%Y-%m-%d"),
+                        as.Date("2019-12-31", "%Y-%m-%d"))
                 )
   )
 }
     
 #' Bar Server Function
-#' 
+#' ¿
 #' The function returns a reactive function that returns a vectors with the
 #'  date of the interval
-#' 
+#'
 #' @param input shiny internal
-#' 
+#'
 #' @param output shiny internal
-#' 
+#'
 #' @param session shiny internal
 #'
-#' @return Reactive function that returns a vector with a time interval 
+#' @return Reactive function that returns a vector with a time interval
 #' (min, max) dates
-mod_bar_server <- function(input, output, session){
+mod_bar_server <- function(input, output, session) {
   ns <- session$ns
   interval_val <- reactive({
     input$filtro_fecha
