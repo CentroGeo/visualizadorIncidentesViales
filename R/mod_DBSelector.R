50d17c16560ae7cb21d5bc4deb211785f09976bd
plablo09 <pablo.lopez@centrogeo.edu.mx>
2021-05-17 17:16:49 -0500

    Mejoras superficiales.

--- R/mod_DBSelector.R
+++ R/mod_DBSelector.R
@@ -1,8 +1,11 @@
 #' DBSelector UI Function
 #'
-#' Generate the UI for selecting the "Incidentes viales" data source,
-#' type of accident and location
-#'
+#' Genera todos los selectortes para filtrar la base de datos.
+#' Tipo de incidente
+#' Base de datos
+#' Periodo de tiempo
+#' Área de análisis
+#' 
 #' @param id shiny id
 #'
 #'
@@ -10,7 +13,7 @@
 #'
 mod_DBSelector_ui <- function(id) {
   ns <- NS(id)
-  ## Vector with the posible locations
+  ## Lista de alcaldías
   list_selec <- c(
     "Total Ciudad de México", "Álvaro Obregón", "Azcapotzalco",
     "Benito Juárez", "Coyoacán", "Cuajimalpa de Morelos",
@@ -18,80 +21,83 @@ mod_DBSelector_ui <- function(id) {
     "La Magdalena Contreras", "Miguel Hidalgo", "Milpa Alta",
     "Tlalpan", "Tláhuac", "Venustiano Carranza", "Xochimilco"
   )
-  ## Vector with the accident types
+  ## Lista de tipos de accidente
   tipo_incidentes <- c("Sin víctimas" = "ACCIDENTE",
                        "Lesionado" = "LESIONADO",
                        "Deceso" = "DECESO")
   bases <- c("FGJ", "SSC", "C5", "AXA")
   fluidPage(
     selectInput(
-      inputId = ns("filtro_lugar"), ## Name to reference in the input
-      label = "Área de Análisis", ##  Label in the UI
-      choices = list_selec, # Vector of choices
-      selected = "Total Ciudad de México" ## Selected by default
+      inputId = ns("filtro_lugar"), ## referencia al valor
+      label = "Área de Análisis",
+      choices = list_selec,
+      selected = "Total Ciudad de México"
     ),
     fluidRow(
       column(
         6,
         checkboxGroupInput(
-          inputId = ns("filtro_incidente"), ## Name to reference in the input
-          label = "Tipo de Incidente", # Label to show in the UI
+          inputId = ns("filtro_incidente"),
+          label = "Tipo de Incidente",
           inline = TRUE,
-          choices = tipo_incidentes, ## Vector of choices
-          selected = "LESIONADO" ## Selected by default
+          choices = tipo_incidentes,
+          selected = "LESIONADO"
         ),
         checkboxGroupInput(
-          inputId = ns("filtro_bd"), ## Name to reference in the input
-          label = "Base de Datos", ## Label to show in the UI
+          inputId = ns("filtro_bd"),
+          label = "Base de Datos",
           inline = TRUE,
-          choices = bases, ## vector of  choices
-          selected = "FGJ" ## Selected by default
+          choices = bases,
+          selected = "FGJ"
         )
       )
     )
   )
 }
 
-
-filtra_datos <- function(dataframe_fil,
+#' filtra_datos
+#'
+#' Filtra los datos de acuerdo a los valores de los selectores.
+#' 
+#' @param datos_filtrados Todos los datos
+#' @param filtro_incidente Tipo de incidente
+#' @param filtro_bd Base de datos
+#' @param filtro_intervalo Intervalo de tiempo
+#' @param filtro_lugar Área de análisis
+#'
+#' @return datos filtrados
+filtra_datos <- function(datos_filtrados,
                          filtro_incidente,
                          filtro_bd,
                          filtro_intervalo,
                          filtro_lugar) {
-  mem_data <- dplyr::filter(dataframe_fil,
-    fuente %in% filtro_bd) %>%
-    dplyr::filter(tipo_incidente %in% filtro_incidente)
-  #### filtro fecha
-  mem_data <- dplyr::filter(
-    mem_data,
-    (mem_data$timestamp > lubridate::ymd(filtro_intervalo[1])
+  datos_filtrados <- dplyr::filter(
+    datos_filtrados,
+    (datos_filtrados$timestamp > lubridate::ymd(filtro_intervalo[1])
     &
-    mem_data$timestamp < lubridate::ymd(filtro_intervalo[2])
+    datos_filtrados$timestamp < lubridate::ymd(filtro_intervalo[2])
     )
   )
+  datos_filtrados <- dplyr::filter(datos_filtrados,
+    fuente %in% filtro_bd) %>%
+    dplyr::filter(tipo_incidente %in% filtro_incidente)
   ### Filter by location (Alcaldias)
   if (filtro_lugar != "Total Ciudad de México") {
-    mem_data <- dplyr::filter(mem_data,
+    datos_filtrados <- dplyr::filter(datos_filtrados,
       nom_mun == filtro_lugar)
   }
-  return(mem_data)
+  return(datos_filtrados)
 }
-#### Uncomment if we want to create the directori un a default manner
-#dir.create("./cache_dir",  showWarnings = TRUE)
-#cache_dir_o <- getOption("Cache_dir", default = "./cache_dir")
 
-
-
-
-fc <- function(cache_selected = "./cache_dir")
-{
+# Esta sólo se encarga de que exista el objeto cache 
+fc <- function(cache_selected = "./cache_dir") {
   dir.create(cache_selected,  showWarnings = FALSE)
   fc_c <- memoise::cache_filesystem(cache_selected)
   print(paste("Path for DB cache Storage: ", cache_selected))
   return (fc_c)
 }
 
-
+# Versión memoizada de filtra_datos
 mem_filtra_datos <- memoise::memoise(
                               filtra_datos,
                               cache = fc(getOption(
@@ -117,7 +123,7 @@ mem_filtra_datos <- memoise::memoise(
 #' @return reactive function that returns a filtered dataframe
 #'  using the inputs from the UI
 mod_DBSelector_server <- function(input, output, session,
-  interval_ba_rea, dataframe_fil) {
+  interval_ba_rea, datos_filtrados) {
     ns <- session$ns
     #### Read
     datafram_re <- reactive({
@@ -125,9 +131,9 @@ mod_DBSelector_server <- function(input, output, session,
       filtro_bd <- input$filtro_bd
       filtro_intervalo <- interval_ba_rea()
       filtro_lugar <- input$filtro_lugar
-      dataframe_fil <- mem_filtra_datos(dataframe_fil, filtro_incidente,
+      datos_filtrados <- mem_filtra_datos(datos_filtrados, filtro_incidente,
         filtro_bd, filtro_intervalo, filtro_lugar)
-      return(dataframe_fil)
+      return(datos_filtrados)
     })
     #datafram_re <- datafram_re %>% shiny::debounce(100)
     seleccion_lugar <- reactive({
