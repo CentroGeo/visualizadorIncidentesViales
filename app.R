1c5354defedc3f1ba4eee25eca8aa287b746aef3
plablo09 <pablo.lopez@centrogeo.edu.mx>
2021-05-19 16:25:57 -0500

    Cache en /tmp (base de datos).

--- app.R
+++ app.R
@@ -1,7 +1,8 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
-
+#options(shiny.maxRequestSize=510*1024^2)
+shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "myapp-cache")))
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 visualizadorIncidentesViales::run_app() # add parameters here (if any)
