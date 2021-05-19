5b422756e8f02d3e82048c4747784c4b44d39a40
plablo09 <pablo.lopez@centrogeo.edu.mx>
2021-05-19 16:43:46 -0500

    Nombre de la carpeta de cache.

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 #options(shiny.maxRequestSize=510*1024^2)
-shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "myapp-cache")))
+shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "vis-viales-cache")))
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 visualizadorIncidentesViales::run_app() # add parameters here (if any)
